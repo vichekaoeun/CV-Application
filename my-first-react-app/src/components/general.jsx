78b4fe1695dbf0a70218c0128b1b982be57e2808
Vicheka Oeun <oeunvicheka95@gmail.com>
2023-12-27 00:03:36 -0500

    add react components

---
+++ my-first-react-app/src/components/general.jsx
@@ -0,0 +1,26 @@
+export default function general() {
+    return (
+        <>
+            <>
+                <h1>Personal Details</h1>
+                <div id='name'>
+                    <h1>Full Name</h1>
+                    <input type="text" />
+                </div>
+                <div id='email'>
+                    <h1>Email</h1>
+                    <input type="text" />
+                </div>
+                <div id='phone'>
+                    <h1>Phone Number</h1>
+                    <input type="text" />
+                </div>
+                <div id='address'>
+                    <h1>Address</h1>
+                    <input type="text" />
+                </div>
+            </>
+
+        </>
+    );
+}
